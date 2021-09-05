cea4c25a86734b62c20e690e8814abe8117fce31
scarter31191 <scarter31191@gmaile.com>
2021-09-05 09:18:35 -0400

    created components folder and added a navabar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './components/navBar';
 
 function App() {
   return (
     <div className="App">
       <h1>from scratch</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function navBar() {
+    return (
+        <div>
+            <h1>NAVBAR</h1>
+        </div>
+    )
+}
+
+export default navBar
