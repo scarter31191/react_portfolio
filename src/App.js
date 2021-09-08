d5527400329b7894f2316e44c153c313f57a9d11
scarter31191 <scarter31191@gmaile.com>
2021-09-08 00:01:52 -0400

    working on navbar route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Navbar from './components/navBar';
+import Home from './components/Home';
+import {Link, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>from scratch</h1>
-      <Navbar />
+      <Router>
+        <div>
+
+        </div>
+      </Router>
+        <Navbar />
     </div>
   );
 }
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import{BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Home from './Home'
 
 function navBar() {
     return (
-        <div>
-            <h1>NAVBAR</h1>
-        </div>
+        <Router>
+            <div>
+                <h1>NAVBAR</h1>
+                    <Switch>
+                           <Route exact path="/" component={Home}/>
+                           <Route to="/about">About</Route>
+                           <Route to="/users">Users</Route>
+                    </Switch>     
+            </div>
+        </Router>
+            
     )
 }
 
